0ee4faf3f7ee1fa7a3f8f84d34773b6d26c1dc49
Juaojoao <soloqf@gmail.com>
2023-01-23 23:39:54 -0300

    Commit V1.0.1

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
-const { Client, GatewayIntentBits, Events, messageCreate } = require('discord.js');
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.GuildMembers,
-    ]
-});
-const config = require("./config.json");
+const { Client, GatewayIntentBits, Events } = require('discord.js');
+const config = require('./config.json')
+const client = new Client({ intents: 38671 });
 
-client.on('ready', () => {
-    console.log(`Bot foi iniciado, com ${client.users.cache.size} usuários, em ${client.channels.cache.size} Canais, em ${client.guilds.cache.size} servidores`);
-    client.user.setPresence({ activities: [{ name: 'Master Mu' }], status: 'online' });
-    client.user.setUsername('MuBot');
-});
-
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-
-    if (interaction.commandName === 'ping') {
-        const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true },);
-        interaction.editReply(`Meu ping está em: **${sent.client.ws.ping}ms.**`);
-    }
+client.on(Events.ClientReady, () => {
+  client.user.setPresence({ 
+    activities: [{ 
+      name: 'Master Mu' 
+    }], 
+    status: 'online' 
+  });
+  client.user.setUsername('MuBot');
+  
+  console.log(`Bot foi iniciado, com ${client.users.cache.size} usuários, em ${client.channels.cache.size} Canais, em ${client.guilds.cache.size} servidores`);
 });
 
-client.on("message", async messageCreate => {
-    console.log(messageCreate)
-    if (messageCreate.content === "clear") {
-        messageCreate.channel.bulkDelete(100)
-            .then(messages => console.log(`Deleted ${messages.size} messages`))
-            .catch(console.error);
-    }
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply(`Meu ping está em: **${client.ws.ping}ms.**`);
+  }
 });
 
+client.on(Events.MessageCreate, async (message) => {
+  if (message.content.startsWith("!clear")) {
+    let args = message.content.split(" ").slice(1);
+    let num = parseInt(args[0]);
+    if(!num) return message.reply("please provide a number of messages to delete")
+    message.channel.bulkDelete(num)
+      .then(messages => console.log(`Bulk deleted ${messages.size} messages`))
+      .catch(console.error);
+  }
+});
 
 client.login(config.token);
