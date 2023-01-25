1bdf2aadb89045cac3a792e0a9fd89112576b360
Juaojoao <soloqf@gmail.com>
2023-01-25 19:54:10 -0300

    Index, Alma do bot

--- index.js
+++ index.js
@@ -1,48 +1,31 @@
-const fs = require("fs");
-const path = require("path");
-const { Client, Collection, GatewayIntentBits, Events } = require("discord.js");
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const { token } = require("./config.json");
+const Discord = require("discord.js");
+const client = new Discord.Client({ intents: [1, 512, 32768, 2, 128] });
+const config = require("./config.json");
 
+module.exports = client;
 
-// filtrar somente arquivos em JS
-client.commands = new Collection();
-const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(".js"));
+// Quando houver uma interação
+client.on("interactionCreate", (interaction) => {
+  if (interaction.type === Discord.InteractionType.ApplicationCommand) {
+    //Gerar comandos usando slashCommands
+    const cmd = client.slashCommands.get(interaction.commandName);
 
-for (const file of commandFiles) {
-  const filePath = path.join(commandsPath, file);
-  const command = require(filePath);
-  if ("data" in command && "execute" in command)
-    client.commands.set(command.data.name, command);
-}
-//
+    if (!cmd) return interaction.reply(`Error`);
 
-// Ao ligar o bot
-client.on(Events.ClientReady, () => {
-  console.log(`👌😎 tudo ok ${client.user.tag}`);
-});
-//
+    interaction["member"] = interaction.guild.members.cache.get(
+      interaction.user.id
+    );
 
-// Interação com os comandos em "/"
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-  const command = interaction.client.commands.get(interaction.commandName);
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-  try {
-    await command.execute(interaction);
-  } catch (err) {
-    console.error(err);
-    await interaction.reply({
-      content: "Ups.. me deparei com um error, tente novamente mais tarde!",
-      ephemeral: true,
-    });
+    cmd.run(client, interaction);
   }
 });
 
-client.login(token);
+client.on("ready", () => {
+  console.log(`🔥 Estou online em ${client.user.username}!`);
+});
+
+client.slashCommands = new Discord.Collection();
+
+require("./handler")(client);
+
+client.login(config.token);
